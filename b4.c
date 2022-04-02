e378c889afc4b23b294c638f5b1c9c088681ceaf
nguoicatba <94635940+nguoicatba@users.noreply.github.com>
2022-04-02 18:09:45 +0700

    Add files via upload

---
+++ b4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+char ms[50];
+void chuhoa(char *s){
+	*s=(char)*s-'a'+'A';
+}
+void chuthuong(char *s){
+	*s=(char)*s-'A'+'a';
+}
+void solve(){
+}
+int main(){
+	solve();
+return 0;
+}
